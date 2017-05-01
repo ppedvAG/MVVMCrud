03d8a27f59e60948ea035ae2f94b2afa0a0f43c4
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-05-01 15:24:58 +0200

    Add MVVM Project

---
+++ MVVMCrud.UserInterface/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVMCrud.UserInterface.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MVVMCrud.UserInterface/ViewModels/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVMCrud.UserInterface.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _executeHandler;
+        private readonly Func<bool> _canExecuteHandler;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException(nameof(execute), "Execute must not be null!");
+
+            _executeHandler = execute;
+        }
+        public RelayCommand(Action execute, Func<bool> canExecute)
+            :this(execute)
+        {
+            _canExecuteHandler = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteHandler == null)
+                return true;
+
+            return _canExecuteHandler();
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeHandler();
+        }
+    }
+}
